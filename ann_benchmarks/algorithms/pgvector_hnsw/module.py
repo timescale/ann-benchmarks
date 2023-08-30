5a7923088ccb6ddbe3d2b90e96085be534ba30bb
Maheedhar PV <mahi@timescale.com>
2023-08-30 15:42:26 +0530

    Support for PGV_HNSW, PGV

--- ann_benchmarks/algorithms/pgvector_hnsw/module.py
+++ ann_benchmarks/algorithms/pgvector_hnsw/module.py
@@ -6,11 +6,11 @@ import psycopg
 
 from ..base.module import BaseANN
 
-
-class PGVector(BaseANN):
-    def __init__(self, metric, lists):
+class PGVectorHNSW(BaseANN):
+    def __init__(self, metric, method_param):
         self._metric = metric
-        self._lists = lists
+        self._m = method_param["M"]
+        self._ef_construction = method_param["efConstruction"]
         self._cur = None
 
         if metric == "angular":
@@ -27,45 +27,41 @@ class PGVector(BaseANN):
         cur = conn.cursor()
         cur.execute("select count(*) from pg_class where relname = 'items'")
         table_count = cur.fetchone()[0]
-
         if table_count == 0:
-            cur.execute("CREATE TABLE items (id int, embedding vector(%d))" % X.shape[1])
+            cur.execute("CREATE TABLE IF NOT EXISTS items (id int, embedding vector(%d))" % X.shape[1])
             cur.execute("ALTER TABLE items ALTER COLUMN embedding SET STORAGE PLAIN")
             print("copying data...")
             with cur.copy("COPY items (id, embedding) FROM STDIN") as copy:
                 for i, embedding in enumerate(X):
                     copy.write_row((i, embedding))
-
+        
         cur.execute("drop index if exists pgv_idx")
         cur.execute("select count(*) from pg_indexes where indexname = 'pgv_idx'")
         index_count = cur.fetchone()[0]
 
-        if index_count == 0:                    
-            print("Creating Index (lists = %d)" % self._lists)
+        if index_count == 0:
+            print("Creating Index (m = %s, ef_construction = %d)" % (self._m, self._ef_construction))
+            cur.execute("SET min_parallel_table_scan_size TO 1")
             if self._metric == "angular":
                 cur.execute(
-                    "CREATE INDEX pgv_idx ON items USING ivfflat (embedding vector_cosine_ops) WITH (lists = %d)" % self._lists
+                    "CREATE INDEX pgv_idx ON items USING hnsw (embedding vector_cosine_ops) WITH (m = %s, ef_construction = %d)" % (self._m, self._ef_construction)
                 )
             elif self._metric == "euclidean":
-                cur.execute("CREATE INDEX pgv_idx ON items USING ivfflat (embedding vector_l2_ops) WITH (lists = %d)" % self._lists)
+                cur.execute("CREATE INDEX pgv_idx ON items USING hnsw (embedding vector_l2_ops) WITH (m = %s, ef_construction = %d)" % (self._m, self._ef_construction))
             else:
                 raise RuntimeError(f"unknown metric {self._metric}")
-            conn.commit()
             print("done!")
         self._cur = cur
 
-    def set_query_arguments(self, probes):
-        self._probes = probes
-        self._cur.execute("SET ivfflat.probes = %d" % probes)
-        print("SET ivfflat.probes = %d" % probes)
-        # TODO set based on available memory
-        self._cur.execute("SET work_mem = '1024MB'")
-        # disable parallel query execution
-        self._cur.execute("SET max_parallel_workers_per_gather = 0")
+    def set_query_arguments(self, ef_search):
+        self._ef_search = ef_search
+        self._cur.execute("SET hnsw.ef_search = %d" % ef_search)
+        print("SET hnsw.ef_search = %d" % ef_search)
+        self._cur.execute("SET work_mem = '4GB'")
 
     def query(self, v, n):
         self._cur.execute(self._query, (v, n), binary=True, prepare=True)
         return [id for id, in self._cur.fetchall()]
 
     def __str__(self):
-        return f"PGVector(lists={self._lists}, probes={self._probes})"
+        return f"PGVectorHNSW(m={self._m}, ef_construction={self._ef_construction}, ef_search={self._ef_search})"
