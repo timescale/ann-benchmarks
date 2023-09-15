a82da215a3cf467c7a5227d234edae9ba465ce54
Maheedhar PV <mahi@timescale.com>
2023-09-15 12:33:31 +0530

    Batch run changes for pg_embedding

--- ann_benchmarks/algorithms/pg_embedding_hnsw/module.py
+++ ann_benchmarks/algorithms/pg_embedding_hnsw/module.py
@@ -1,17 +1,16 @@
 import subprocess
 import sys
-
 import psycopg
-
 from ..base.module import BaseANN
-
+from   psycopg_pool import ConnectionPool
 
 class PGEmbeddingHNSW(BaseANN):
     def __init__(self, metric, method_param):
         self._metric = metric
         self._m = method_param["M"]
         self._ef_construction = method_param["efConstruction"]
-        self._cur = None
+        self._pool = None
+        self._ef = None
 
         if metric == "angular":
             self._query = "SELECT id FROM items ORDER BY embedding <=> %s::real[] LIMIT %s"
@@ -20,59 +19,83 @@ class PGEmbeddingHNSW(BaseANN):
         else:
             raise RuntimeError(f"unknown metric {metric}")
 
+    def start_pool(self):
+        def configure(conn):
+            #pgvector.psycopg.register_vector(conn)
+            if self._ef is not None:
+                conn.execute("ALTER INDEX items_embedding_idx  SET (efsearch=%d)" % self._ef)
+                print("ALTER INDEX items_embedding_idx SET (efsearch=%d)" % self._ef)
+                # disable parallel query execution
+                conn.execute("SET work_mem = '8GB'")
+                # disable parallel query execution
+                conn.execute("SET max_parallel_workers_per_gather = 0")
+                conn.execute("SET enable_seqscan=0")
+            conn.commit()
+
+        self._pool = ConnectionPool("postgresql://ann:ann@localhost/ann",
+                                    min_size=1, max_size=16, configure=configure)
+
     def fit(self, X):
-        #subprocess.run("service postgresql start", shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr)
-        conn = psycopg.connect(user="ann", password="ann", dbname="ann", host="localhost")
-        cur = conn.cursor()
-        cur.execute("select count(*) from pg_class where relname = 'items'")
-        table_count = cur.fetchone()[0]
+        self.start_pool()
+        with self._pool.connection() as conn:
+            cur = conn.cursor()
+            cur.execute("select count(*) from pg_class where relname = 'items'")
+            table_count = cur.fetchone()[0]
 
-        if table_count == 0:
-            cur.execute("CREATE TABLE items (id int, embedding real[])")
-            cur.execute("ALTER TABLE items ALTER COLUMN embedding SET STORAGE PLAIN")
-            print("copying data...")
-            with cur.copy("COPY items (id, embedding) FROM STDIN") as copy:
-                for i, embedding in enumerate(X):
-                    copy.write_row((i, embedding.tolist()))
-        
-        cur.execute("drop index if exists items_embedding_idx")
-        cur.execute("select count(*) from pg_indexes where indexname = 'items_embedding_idx'")
-        index_count = cur.fetchone()[0]
+            if table_count == 0:
+                cur.execute("CREATE TABLE items (id int, embedding real[])")
+                cur.execute("ALTER TABLE items ALTER COLUMN embedding SET STORAGE PLAIN")
+                conn.commit()
+                print("copying data...")
+                with cur.copy("COPY items (id, embedding) FROM STDIN") as copy:
+                    for i, embedding in enumerate(X):
+                        copy.write_row((i, embedding.tolist()))
+                cur.execute("COMMIT")
 
-        if index_count == 0:
-            print("Creating Index (dims=%d, m=%d, efconstruction=%d)" % (X.shape[1], self._m, self._ef_construction))
-            if self._metric == "angular":
-                cur.execute(
-                    "CREATE INDEX items_embedding_idx ON items USING hnsw (embedding ann_cos_ops) WITH (dims=%d, m = %d, efconstruction = %d)" % (X.shape[1], self._m, self._ef_construction)
-                )
-            elif self._metric == "euclidean":
-                cur.execute(
-                    "CREATE INDEX items_embedding_idx ON items USING hnsw (embedding ann_l2_ops) WITH (dims=%d, m = %d, efconstruction = %d)" % (X.shape[1], self._m, self._ef_construction)
+            cur.execute("drop index if exists items_embedding_idx")
+            cur.execute("select count(*) from pg_indexes where indexname = 'items_embedding_idx'")
+            index_count = cur.fetchone()[0]
+
+            if index_count == 0:
+                cur.execute("SET maintenance_work_mem = '16GB'")
+                print("Creating Index (dims=%d, m=%d, efconstruction=%d)" % (X.shape[1], self._m, self._ef_construction))
+                if self._metric == "angular":
+                    cur.execute(
+                        "CREATE INDEX items_embedding_idx ON items USING hnsw (embedding ann_cos_ops) WITH (dims=%d, m = %d, efconstruction = %d)" % (X.shape[1], self._m, self._ef_construction)
                     )
-            else:
-                raise RuntimeError(f"unknown metric {self._metric}")
-            print("Index construction done!")
-            #print("prewarming index...")
-            #cur.execute("SELECT pg_prewarm('items_embedding_idx', 'buffer')")
-            #print("Index prewarming done!")
-        self._cur = cur
+                elif self._metric == "euclidean":
+                    cur.execute(
+                        "CREATE INDEX items_embedding_idx ON items USING hnsw (embedding ann_l2_ops) WITH (dims=%d, m = %d, efconstruction = %d)" % (X.shape[1], self._m, self._ef_construction)
+                        )
+                else:
+                    raise RuntimeError(f"unknown metric {self._metric}")
+                # reset back to the default value after index creation
+                cur.execute("SET maintenance_work_mem = '2GB'")
+                conn.commit()
+            print("prewarming index...")
+            cur.execute("SELECT pg_prewarm('items_embedding_idx', 'buffer')")
+            print("Index prewarming done!")
 
     def set_query_arguments(self, ef):
         self._ef = ef
-        self._cur.execute("ALTER INDEX items_embedding_idx  SET (efsearch=%d)" % ef)
-        print("ALTER INDEX items_embedding_idx SET (efsearch=%d)" % ef)
-        # disable parallel query execution
-        self._cur.execute("SET max_parallel_workers_per_gather = 0")
+        #close and restart the pool to apply the new settings
+        self._pool.close()
+        self._pool = None
+        self.start_pool()
+ 
+    def get_memory_usage(self):
+        if self._pool is None:
+            return 0
+        with self._pool.connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT pg_relation_size('items_embedding_idx')")
+                return cursor.fetchone()[0] / 1024
 
     def query(self, v, n):
-        self._cur.execute(self._query, (v.tolist(), n), binary=True, prepare=True)
-        return [id for id, in self._cur.fetchall()]
-
-    def get_memory_usage(self):
-        if self._cur is None:
-            return 0
-        self._cur.execute("SELECT pg_relation_size('items_embedding_idx')")
-        return self._cur.fetchone()[0] / 1024
+        with self._pool.connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(self._query, (v.tolist(), n), binary=True, prepare=True)
+                return [id for id, in cursor.fetchall()]
 
     def __str__(self):
         return f"PGEmbedding(m={self._m}, probes={self._ef_construction}, query_ef_search={self._ef})"
