3345ca25fb13021ccac1969d7f1bdd3a7dd0d950
John Pruitt <jgpruitt@gmail.com>
2023-11-02 17:49:38 -0500

    performance improvement to pinecone

--- ann_benchmarks/algorithms/pinecone/module.py
+++ ann_benchmarks/algorithms/pinecone/module.py
@@ -7,13 +7,14 @@ import numpy
 from time import sleep
 
 class Pinecone(BaseANN):
-    def __init__(self, metric, api_key, environment, index_name, pods, pod_type):
+    def __init__(self, metric, api_key, environment, index_name, pods, pod_type, replicas):
         self._metric = metric
         self._api_key = api_key
         self._environment = environment
         self._index_name = index_name
         self._pods = pods
         self._pod_type = pod_type
+        self._replicas = replicas
         self._query_search_list_size = None
 
     def done(self) -> None:
@@ -27,13 +28,15 @@ class Pinecone(BaseANN):
         print("initializing pinecone client...")
         pinecone.init(api_key=self._api_key, environment=self._environment)
         dimension = X.shape[1]
+        print(f"dimension: {dimension}")
         for idx in pinecone.list_indexes():
             if idx == self._index_name:
                 print(f"deleting existing index {self._index_name}...")
                 pinecone.delete_index(self._index_name)
         print(f"creating index {self._index_name}...")
         pinecone.create_index(name=self._index_name, dimension=dimension, 
-                              metric=self._metric, pods=self._pods, pod_type=self._pod_type)
+                              metric=self._metric, pods=self._pods, 
+                              pod_type=self._pod_type, replicas=self._replicas)
         print("waiting for index to be ready...")
         ready = False
         while not ready:
@@ -43,6 +46,7 @@ class Pinecone(BaseANN):
         print("upserting dataset...")
         index = pinecone.Index(self._index_name)
         total = len(X)
+        print(f"upserting {total} vectors...")
         batch = []
         for i, v in enumerate(X):
             batch.append({"id": str(i), "values": v.astype(float).tolist()})
@@ -51,10 +55,10 @@ class Pinecone(BaseANN):
                 index.upsert(vectors=batch, batch_size=len(batch), show_progress=True)
                 batch = []
         print("index loaded")
+        self._index = index
 
     def query(self, q: numpy.array, n: int) -> numpy.array:
-        index = pinecone.Index(self._index_name)
-        resp = index.query(vector=q.astype(float).tolist(), top_k=n, include_values=False)
+        resp = self._index.query(vector=q.astype(float).tolist(), top_k=n, include_values=False)
         matches = []
         for match in resp['matches']:
             matches.append(int(match['id']))
